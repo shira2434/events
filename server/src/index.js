ec0d60b2b6307f2570f9c48bdaf02a2cfa221836
shira2434 <shirameren2434@gmail.com>
2026-04-29 07:49:45 +0300

    initial commit

--- server/src/index.js
+++ server/src/index.js
@@ -18,5 +18,13 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
+// Serve React build in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
